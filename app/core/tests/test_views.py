4ed5dbf89d836d980ba38096d94c5fae254c75a9
henry <aengrhenry@gmail.com>
2024-07-23 09:00:59 +0100

    orders test[add unit testing for placing order endpoint and order history endpoint]

--- app/core/tests/test_views.py
+++ app/core/tests/test_views.py
@@ -17,6 +17,7 @@ class ProductTests(APITestCase):
         }
         self.product = Product.objects.create(**self.product_data)
         self.admin_user = User.objects.create_superuser(email='admin@example.com', password='adminpassword')
+        self.user = User.objects.create_user(name='testuser', email="testuser01@example.com", password='userpassword')
 
     def get_token(self, user):
         refresh = RefreshToken.for_user(user)
@@ -55,3 +56,26 @@ class ProductTests(APITestCase):
         self.product.refresh_from_db()
         self.assertEqual(self.product.name, 'Updated Laptop')
         self.assertEqual(self.product.description, 'An updated powerful laptop')
+
+    def test_order_product_endpoints(self):
+        token = self.get_token(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        cart_item={
+            'product':self.product.id,
+            'quantity':1
+        }
+        add_to_cart = self.client.post('/api/orders/add-to-cart', cart_item, format='json')
+        order = self.client.post('/api/orders/place-order')
+        self.assertEqual(add_to_cart.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(add_to_cart.data['quantity'], cart_item['quantity'])
+        self.assertEqual(order.status_code, status.HTTP_200_OK)
+        self.assertEqual(order.data['message'], 'order place successfully')
+
+
+    def test_get_order_history_endpoint(self):
+        token = self.get_token(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        response = self.client.get('/api/orders/order-history')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+
